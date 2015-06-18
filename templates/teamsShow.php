ba24a64737eee315b36dd496c8977a24249474aa
bat16 <bastienbourdin@gmail.com>
2015-06-18 22:03:47 +0200

    changement de couleur pour la page Team-show

--- templates/teamsShow.php
+++ templates/teamsShow.php
@@ -2,14 +2,14 @@
 	<span class="go-back-arrow control ion-ios-arrow-thin-left" target="<?php secho($this->generateUrl('ranking')); ?>" target-id="ranking" animation="slideInLeft" ></span>
 	<h1><?php secho($team['name']); ?> - <?php secho($points); ?> points</h1>
 
-	<div class="icons-top">	</div>
+	<div class="icons-top"> </div>
 
 	<?php if (!count($users)) { ?>
 		<div class="no-data">Pas de données</div>
 	<?php } else { ?>
 		<div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3 infos-container">
 			<span class="ion-ios-circle-filled infos-container-round" ></span>
-			<span class="ion-ios-person" id="infos-container-teams-user" ></span>
+			<span class="ion-ios-person" id="infos-container-teams-show-user" ></span>
 			<?php foreach ($users as $user) { ?>
 				<div class="info">
 					<span class="control col-xs-12" target="<?php secho($this->generateUrl('users', 'show', [$user['id'], TRUE])); ?>" target-id="users-show"><?php secho($user['firstname']." ".$user['lastname']); ?></span>
