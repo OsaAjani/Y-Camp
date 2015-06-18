cfd4b40d582c7021f787a74b89acbcb2058ca782
bat16 <bastienbourdin@gmail.com>
2015-06-18 21:30:05 +0200

    mise en place du boutton Retour vers la page TeamShow

--- templates/teamsShow.php
+++ templates/teamsShow.php
@@ -12,7 +12,7 @@
 			<span class="ion-ios-person" id="infos-container-teams-user" ></span>
 			<?php foreach ($users as $user) { ?>
 				<div class="info">
-					<span class="control col-xs-12" target="<?php secho($this->generateUrl('users', 'show', [$user['id'], 'show'])); ?>" target-id="users-show"><?php secho($user['firstname']." ".$user['lastname']); ?></span>
+					<span class="control col-xs-12" target="<?php secho($this->generateUrl('users', 'show', [$user['id'], TRUE])); ?>" target-id="users-show"><?php secho($user['firstname']." ".$user['lastname']); ?></span>
 				</div>
 				<div class="clearfix"></div>
 			<?php } ?>
