572b96e0ccc8015bddd990eaf5563af9f7b7a571
bat16 <bastienbourdin@gmail.com>
2015-06-18 13:32:38 +0200

    modification css + ajout information user sur la page usersShow

---
+++ templates/usersShow.php
@@ -0,0 +1,23 @@
+<div id="users-show" class="tile">
+	<span class="go-back-arrow control ion-ios-arrow-thin-left" target="<?php secho($this->generateUrl('teams')); ?>" target-id="teams" animation="slideInLeft" ></span>
+	<h1><?php secho($user['firstname']." ".$user['lastname']); ?></h1>
+
+	<div class="text-center">
+	<div class="img-users overflow">
+		<img alt="photo d'identitée" src="<?php echo HTTP_PWD_IMG . "users/" . $user['id'] . "_" . strtolower($user['firstname']) . "_" . strtolower($user['lastname']).'.jpg'; ?>" />
+	</div>
+	</div>
+	<div class="info-user text-center">
+		<label class="col-xs-12 col-md-6 md-right bold"> Sexe : </label><span class="col-xs-12 col-md-6 md-left no-padding" ><?php echo $user['sexe'] ? 'Homme' : 'Femme'; ?></span>
+		<div class="clearfix"></div>
+		<label class="col-xs-12 col-md-6 md-right bold"> Date de naissance : </label><span class="col-xs-12 col-md-6 md-left no-padding" ><?php secho($birthdate) ; ?></span>
+		<div class="clearfix"></div>
+		<label class="col-xs-12 col-md-6 md-right bold"> Promotion : </label><span class="col-xs-12 col-md-6 md-left no-padding" ><?php secho($user['promotion']) ; ?></span>
+		<div class="clearfix"></div>
+		<label class="col-xs-12 col-md-6 md-right bold"> École : </label><span class="col-xs-12 col-md-6 md-left no-padding" ><?php secho($user['school']) ; ?></span>
+		<div class="clearfix"></div>
+		<label class="col-xs-12 col-md-6 md-right bold"> Dernier diplôme : </label><span class="col-xs-12 col-md-6 md-left no-padding" ><?php secho($user['studies']) ; ?></span>
+		<div class="clearfix"></div>
+		<label class="col-xs-12 col-md-6 md-right bold"> ville : </label><span class="col-xs-12 col-md-6 md-left no-padding" ><?php secho($user['city']) ; ?></span>
+	</div>
+</div>
