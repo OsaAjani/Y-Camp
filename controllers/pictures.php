184d4e7e572055dbb23864b005e666fb5cb2f4cf
bat16 <bastienbourdin@gmail.com>
2015-06-19 21:23:31 +0200

    ajout gestion affichage photo

--- controllers/pictures.php
+++ controllers/pictures.php
@@ -6,10 +6,15 @@ class pictures extends Controller
 {
 	/**
 	 * Page de pictures par défaut
+	 * @param $tpictureName : nom de l'iamge choisis par le user
+	 * @param $teamId : l'id de la team
 	 */	
-	public function show ()
+	public function show ($pictureName, $teamId)
 	{
-
+		return $this->render("picturesShow", array(
+			'teamId' => $teamId,
+			'pictureName' => $pictureName,
+		));
 	}
 
 }
