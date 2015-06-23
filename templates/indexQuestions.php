8cf105e28669e6249cc3b325a858fd84099889ac
root <root@ajani.hd.free.fr>
2015-06-23 14:32:16 +0200

    Mise à jour du style de la page questions

--- templates/indexQuestions.php
+++ templates/indexQuestions.php
@@ -1,14 +1,21 @@
 <div id="index-questions" class="tile">
 	<span class="go-back-arrow control ion-ios-arrow-thin-left" target="<?php secho($this->generateUrl('index', 'email')); ?>" target-id="index-email" animation="slideInLeft" ></span>
 	<h1>Foire aux questions</h1>
+	
+	<div id="index-questions-container" class="col-xs-10 col-xs-offset-1">
+		<h3 class="index-questions-question">Je n'arrive pas à me connecter, que faire ?</h3>
+		<div class="index-questions-reponse">
+			Haec subinde Constantius audiens et quaedam referente Thalassio doctus, quem eum odisse iam conpererat lege communi, scribens ad Caesarem blandius adiumenta paulatim illi subtraxit, sollicitari se simulans ne, uti est militare otium fere tumultuosum, in eius perniciem conspiraret, solisque scholis iussit esse contentum palatinis et protectorum cum Scutariis et Gentilibus, et mandabat Domitiano, ex comite largitionum, praefecto ut cum in Syriam venerit, Gallum, quem crebro acciverat, ad Italiam properare blande hortaretur et verecunde.
+		</div>
 
-	<div class="icons-top"> </div>
-	<div class="info-user">
-		<label class="col-xs-12 bold">Je n'arrive pas à me connecter, pourquoi ? </label><span class="col-xs-12 text-right" >En cas de problème, veulliez contacter les responsables de l'evenement.</span>
-		<div class="clearfix"></div>
-		<label class="col-xs-12"> Adresse campus : </label><span class="col-xs-12 text-right" >89, quai des Chartrons</span>
-		<div class="clearfix"></div>
-		<label class="col-xs-12 bold"> Arrêt de tram le plus proche : </label><span class="col-xs-12 text-right" >Tram B - Les Chartrons</span>
-		<div class="clearfix"></div>
+		<h3 class="index-questions-question">À qui doit-on ramener les objets ?</h3>
+		<div class="index-questions-reponse">
+			Intellectum est enim mihi quidem in multis, et maxime in me ipso, sed paulo ante in omnibus, cum M. Marcellum senatui reique publicae concessisti, commemoratis praesertim offensionibus, te auctoritatem huius ordinis dignitatemque rei publicae tuis vel doloribus vel suspicionibus anteferre. 
+		</div>
+
+		<h3 class="index-questions-question">Quelles technologies sont utilisées par l'application ?</h3>
+		<div class="index-questions-reponse">
+			Ego vero sic intellego, Patres conscripti, nos hoc tempore in provinciis decernendis perpetuae pacis habere oportere rationem. Nam quis hoc non sentit omnia alia esse nobis vacua ab omni periculo atque etiam suspicione belli ?
+		</div>
 	</div>
 </div>
