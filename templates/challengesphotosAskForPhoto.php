728dcd6ecb9bcb49c2cbb5a7ba69ef1cacfca420
root <root@ajani.hd.free.fr>
2015-06-23 17:14:49 +0200

    Fix de l'affichage des alertes sur le formulaire de photo

--- templates/challengesphotosAskForPhoto.php
+++ templates/challengesphotosAskForPhoto.php
@@ -1,8 +1,8 @@
 <div class="text-center">
-	<label for="photo-input">
-		<span id="photo-button-camera" class="ion-ios-camera-outline" ></span>
-	</label>
+	<form class="ajax-form" action="<?php secho($this->generateUrl('challengesphotos', 'valid', [$challenge['id'], $_SESSION['csrf']])); ?>" method="POST" id="photo-add-form" enctype="multipart/form-data" target="<?php secho($this->generateUrl('challengesphotos', 'confirmvalid', [$challenge['id'], $_SESSION['csrf']])); ?>" target-id="challengesphotos-confirmvalid">
+		<label for="photo-input">
+			<span id="photo-button-camera" class="ion-ios-camera-outline" ></span>
+		</label>
+		<input id="photo-input" type="file" name="photo" accept="image/*;capture=camera" />
+	</form>
 </div>
-<form class="ajax-form" action="<?php secho($this->generateUrl('challengesphotos', 'valid', [$challenge['id'], $_SESSION['csrf']])); ?>" method="POST" id="photo-add-form" enctype="multipart/form-data" target="<?php secho($this->generateUrl('challengesphotos', 'confirmvalid', [$challenge['id'], $_SESSION['csrf']])); ?>" target-id="challengesphotos-confirmvalid">
-	<input id="photo-input" type="file" name="photo" accept="image/*;capture=camera" />
-</form>
