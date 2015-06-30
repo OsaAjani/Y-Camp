ab6cbaa95731e6dbeb96b339aac86c7bfb426198
root <root@ajani.hd.free.fr>
2015-06-30 16:34:02 +0200

    Use CDN

--- templates/incs/head.php
+++ templates/incs/head.php
@@ -10,10 +10,7 @@
 		<link rel="icon" type="image/png" href="<?php echo HTTP_PWD_IMG; ?>favicon.ico" />
 
 		<!-- CSS  -->
-		<link rel="stylesheet" type="text/css" href="<?php echo HTTP_PWD_CSS; ?>bootstrap.min.css" />
-		<link rel="stylesheet" type="text/css" href="<?php echo HTTP_PWD_CSS; ?>bootstrap.min.css" />
-		<link rel="stylesheet" type="text/css" href="<?php echo HTTP_PWD_CSS; ?>bootstrap-slider.css" />
-		<link rel="stylesheet" type="text/css" href="<?php echo HTTP_PWD_CSS; ?>responsive-font.min.css" />
+		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />
 		<link rel="stylesheet" type="text/css" href="<?php echo HTTP_PWD_CSS; ?>animate.css" />
 		<link rel="stylesheet" type="text/css" href="<?php echo HTTP_PWD_CSS; ?>style.css" />
 		<link href='https://fonts.googleapis.com/css?family=Raleway:400,100,200,300,600,500,800,900,700' rel='stylesheet' type='text/css'>
@@ -21,8 +18,8 @@
 
 		<!-- JS  -->
 		<script type="text/javascript" src="<?php echo $this->generateUrl('phptojs'); ?>"></script>
-		<script type="text/javascript" src="<?php echo HTTP_PWD_JS; ?>jquery-2.1.1.min.js"></script>
-		<script type="text/javascript" src="<?php echo HTTP_PWD_JS; ?>bootstrap.min.js"></script>
+		<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 		<script type="text/javascript" src="<?php echo HTTP_PWD_JS; ?>custom.js"></script>
 		
 	</head>
